5dacd3e01d3f45e7ff5198508f9a8e132a306407
Ismael Benito <isman7@gmail.com>
2018-01-04 17:28:42 +0100

    PlatformIO called with CliRunner instead of subprocess.

--- gyro/device/main.py
+++ gyro/device/main.py
@@ -1,6 +1,10 @@
-from subprocess import Popen, PIPE
+from __future__ import print_function
 import os
 import configparser as cfgp
+from platformio.commands.run import cli
+import pprint
+import StringIO
+from click.testing import CliRunner
 
 submodule_path = os.path.dirname(__file__)
 module_path = os.path.dirname(submodule_path)
@@ -12,43 +16,39 @@ class Board(object):
 
         self.firmware_path = kwargs.pop("firmware_path", None)
         if not self.firmware_path:
-            self.firmware = kwargs.pop("firmware", "Serial_to_I2C")
+            self.firmware = kwargs.pop("firmware", "sample")
             self.firmware_path = os.path.join(module_path, "firmwares", self.firmware)
         print(self.firmware_path)
-        self.environment = kwargs.pop("environment", "uno")
-        self.upload_port = kwargs.pop("upload_port", None)
         self.env_parser = cfgp.ConfigParser()
-        if not self.upload_port:
-            try:
-                with open(os.path.join(self.firmware_path, "platformio.ini"), "r") as pio_ini:
-                    self.env_parser.read_file(pio_ini)
+        self.pio_cli = cli
 
-                self.upload_port = self.env_parser.get("env:{}".format(self.environment), "upload_port")
+        INI_FILE_PATH = os.path.join(self.firmware_path, "platformio.ini")
 
-            except Exception as e:
-                print("No configurable upload_port", e)
+        if os.path.isfile(INI_FILE_PATH):
+            with open(INI_FILE_PATH, "r") as pio_ini:
+                self.env_parser.read_file(pio_ini)
 
-    def upload(self, **kwargs):
+        else:
+            print("No platformio.ini file found, upoload will fail.")
 
-        folder_path = kwargs.pop("firmware_path", self.firmware_path)
-        env = kwargs.pop("environment", self.environment)
+    def __repr__(self):
+        properties_dict = {key: dict(item) for key, item in dict(self.env_parser).items()}
+        # TODO custom better pretty print fuction...
+        repr_stream = StringIO.StringIO()
+        pretty_printer = pprint.PrettyPrinter(indent=4, stream=repr_stream)
+        pretty_printer.pprint(properties_dict)
+        repr_stream.seek(0)
+        return "Board with build project: \n" + repr_stream.read()
 
-        arguments = [
-            "platformio", "run",
-            "-t", "upload",
-            "-d", folder_path,
-            "-e", env,
-        ]
+    def upload(self):
 
-        platformio_process = Popen(arguments, stdout=PIPE)
-        platformio_process.wait()
-        out = platformio_process.stdout.read()
-        platformio_process.stdout.close()
-        return out
-
-    def check(self):
-        # TODO check that port exists, etc.
-        pass
+        CURRENT_PATH = os.path.abspath(os.curdir)
+        os.chdir(self.firmware_path)
+        # TODO find what Python is CliRunner using.
+        runner = CliRunner()
+        result = runner.invoke(self.pio_cli, ["--target", 'upload'])
+        print(result.output)
+        os.chdir(CURRENT_PATH)
 
 
 

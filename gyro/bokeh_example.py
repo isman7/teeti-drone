63cc8d16e544a173caff5644c148403523b72373
Ismael Benito <isman7@gmail.com>
2018-01-08 14:44:30 +0100

    Standalone mode for bokeh.

--- gyro/bokeh_example.py
+++ gyro/bokeh_example.py
@@ -1,16 +1,18 @@
 # myplot.py
 from __future__ import print_function
-from bokeh.plotting import figure, curdoc
-from bokeh.driving import linear, count
-import random
 
+from tornado.ioloop import IOLoop
 
-import serial
-import json
+from bokeh.application.handlers import FunctionHandler
+from bokeh.application import Application
+from bokeh.server.server import Server
+from bokeh.plotting import figure
+from bokeh.driving import count
+
 from gyro.device import Board
 import numpy as np
-import pyqtgraph as pg
 
+io_loop = IOLoop.current()
 
 board = Board(firmware="teeti-MPU9250-firmware")
 print(board)
@@ -18,27 +20,36 @@ print(board)
 
 board.connect()
 
-p = figure(plot_width=400, plot_height=400)
-r1 = p.line(np.arange(100), np.zeros(100), color="firebrick", line_width=2)
-r2 = p.line(np.arange(100), np.zeros(100), color="navy", line_width=2)
 
-ds1 = r1.data_source
-ds2 = r2.data_source
+def modify_doc(doc):
+    p = figure(plot_width=400, plot_height=400)
+    r1 = p.line(np.arange(100), np.zeros(100), color="firebrick", line_width=2)
+    r2 = p.line(np.arange(100), np.zeros(100), color="navy", line_width=2)
 
+    ds1 = r1.data_source
+    ds2 = r2.data_source
 
-@count()
-def update(step):
+    @count()
+    def update(t):
 
-    ds1.data['y'] = board.read_z()
-    # ds1.data['y'].append(random.randint(0,100))
-    # ds2.data['x'].append(step)
-    # ds2.data['y'].append(random.randint(0,100))
-    ds1.trigger('data', ds1.data, ds1.data)
-    ds2.trigger('data', ds2.data, ds2.data)
+        ds1.data['y'] = board.read_z()
+        # ds1.data['y'].append(random.randint(0,100))
+        # ds2.data['x'].append(step)
+        # ds2.data['y'].append(random.randint(0,100))
+        ds1.trigger('data', ds1.data, ds1.data)
+        ds2.trigger('data', ds2.data, ds2.data)
 
-curdoc().add_root(p)
+    doc.add_root(p)
 
-# Add a periodic callback to be run every 500 milliseconds
-curdoc().add_periodic_callback(update, 100)
+    # Add a periodic callback to be run every 500 milliseconds
+    doc.add_periodic_callback(update, 100)
 
+bokeh_app = Application(FunctionHandler(modify_doc))
 
+server = Server({'/': bokeh_app}, io_loop=io_loop)
+server.start()
+
+if __name__ == '__main__':
+    print('Opening Bokeh application on http://localhost:5006/')
+    io_loop.add_callback(server.show, "/")
+    io_loop.start()

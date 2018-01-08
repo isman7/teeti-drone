a497b9240678931c4d909b7675b15803fe55aa10
Ismael Benito <isman7@gmail.com>
2018-01-08 16:17:24 +0100

    Simple test to plot other variables.

--- gyro/bokeh_server.py
+++ gyro/bokeh_server.py
@@ -1,16 +1,15 @@
-# myplot.py
 from __future__ import print_function
 
 from tornado.ioloop import IOLoop
-
 from bokeh.application.handlers import FunctionHandler
 from bokeh.application import Application
 from bokeh.server.server import Server
 from bokeh.plotting import figure
 from bokeh.driving import count
 
-from gyro.device import Board
 import numpy as np
+from gyro.device import Board
+
 
 io_loop = IOLoop.current()
 
@@ -25,21 +24,24 @@ def modify_doc(doc):
     p = figure(plot_width=400, plot_height=400)
     r1 = p.line(np.arange(100), np.zeros(100), color="firebrick", line_width=2)
     r2 = p.line(np.arange(100), np.zeros(100), color="navy", line_width=2)
+    r3 = p.line(np.arange(100), np.zeros(100), color="green", line_width=2)
 
     ds1 = r1.data_source
     ds2 = r2.data_source
+    ds3 = r3.data_source
 
     @count()
     def update(t):
 
         updated_dataframe = board.read()
 
-        ds1.data['y'] = np.array(updated_dataframe["acc"]["z"])
-        # ds1.data['y'].append(random.randint(0,100))
-        # ds2.data['x'].append(step)
-        # ds2.data['y'].append(random.randint(0,100))
+        ds1.data['y'] = np.array(updated_dataframe["acc"]["x"])
+        ds2.data['y'] = np.array(updated_dataframe["acc"]["z"])
+        ds3.data['y'] = np.array(updated_dataframe["acc"]["y"])
+
         ds1.trigger('data', ds1.data, ds1.data)
         ds2.trigger('data', ds2.data, ds2.data)
+        ds3.trigger('data', ds3.data, ds3.data)
 
     doc.add_root(p)
 
